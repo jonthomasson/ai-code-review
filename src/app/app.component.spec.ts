59c1a22923427d8fe579d5a4d0106939abce6198
Jon Thomasson <jthomasson@sjcoe.net>
2024-09-12 08:10:06 -0700

    updating project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'github-ai-review' title`, () => {
+  it(`should have the 'ai-code-review' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('github-ai-review');
+    expect(app.title).toEqual('ai-code-review');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, github-ai-review');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ai-code-review');
   });
 });
