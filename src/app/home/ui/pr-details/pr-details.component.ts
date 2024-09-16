11a378768f4e5f520eb903fb669ef858a2a7b770
Jon Thomasson <jthomasson@sjcoe.net>
2024-09-15 21:40:16 -0700

    some ui updates

---
+++ src/app/home/ui/pr-details/pr-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pr-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './pr-details.component.html',
+  styleUrl: './pr-details.component.css'
+})
+export class PrDetailsComponent {
+
+}
---
+++ src/app/home/ui/pr-view/pr-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pr-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './pr-view.component.html',
+  styleUrl: './pr-view.component.css'
+})
+export class PrViewComponent {
+
+}
