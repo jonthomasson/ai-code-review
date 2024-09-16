f3f278541cb5c0a543148338fce9785530e1ee19
Jon Thomasson <jthomasson@sjcoe.net>
2024-09-16 12:51:45 -0700

    fixing auth guard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './auth.guard';  
+import { AuthGuard } from '@angular/fire/auth-guard';
 import { LoginComponent } from './login/login.component';
 
 export const appRoutes: Routes = [
