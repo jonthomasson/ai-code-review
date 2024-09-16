9f0c6506f2a4a85728aafa16a4b1be0f990f13d6
Jon Thomasson <jthomasson@sjcoe.net>
2024-09-16 12:16:58 -0700

    updating navbar logo

--- src/app/home/ui/navbar/navbar.component.ts
+++ src/app/home/ui/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../shared/services/auth.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
