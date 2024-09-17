1aadf0ddfb27ca2399562af5e2fa84f69a1e2ad4
Jon Thomasson <jthomasson@sjcoe.net>
2024-09-17 16:25:05 -0700

    adding signals to github service.

--- src/app/shared/services/github.service.ts
+++ src/app/shared/services/github.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, catchError, filter, forkJoin, map, of, shareReplay, switchMap, throwError } from 'rxjs';
+import { toSignal, toObservable } from '@angular/core/rxjs-interop';
 import { GitHubRepository, GitHubPullRequest, GitHubPullRequestFile } from '@shared/models/github';
 import { AuthService } from '@shared/services/auth.service';
 
@@ -9,8 +10,40 @@ import { AuthService } from '@shared/services/auth.service';
 })
 export class GithubService {
   apiBase: string = 'https://api.github.com';
+  http = inject(HttpClient);
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private authService: AuthService) { }
+
+  headers = this.createHeaders();
+
+  //expose observables as private so that they can only be subscribed here by our signals.
+  //repositories
+  private repositories$ = this.http.get<GitHubRepository[]>(`${this.apiBase}/user/repos?per_page=100&type=all`, { headers: this.headers }).pipe(
+    shareReplay(1),
+    catchError(this.handleError)
+  );
+  repositories = toSignal(this.repositories$, { initialValue: [] as GitHubRepository[] });
+  selectedRepository = signal<GitHubRepository | undefined>(undefined);
+
+  //pull requests
+  private pullRequests$ = toObservable(this.selectedRepository).pipe(
+    filter(Boolean),
+    switchMap(repo =>
+      this.http.get<GitHubPullRequest[]>(`${this.apiBase}/repos/${repo.owner}/${repo.name}/pulls`, { headers: this.headers }
+      )
+    ));
+  pullRequests = toSignal(this.pullRequests$, { initialValue: [] as GitHubPullRequest[] });
+  selectedPullRequest = signal<GitHubPullRequest | undefined>(undefined);
+
+  //pull request files
+  private pullRequestFiles$ = toObservable(this.selectedPullRequest).pipe(
+    filter(Boolean),
+    switchMap(pull =>
+      this.http.get<GitHubPullRequestFile[]>(`${this.apiBase}/repos/${pull.base.user.login}/${pull.base.repo.name}/pulls/${pull.number}/files`, { headers: this.headers }
+      )
+    ));
+  pullRequestFiles = toSignal(this.pullRequestFiles$, { initialValue: [] as GitHubPullRequestFile[] });
+
 
   private createHeaders(): HttpHeaders {
     let headers = new HttpHeaders();
@@ -27,8 +60,8 @@ export class GithubService {
   }
 
   getGitHubRepositories(): Observable<GitHubRepository[]> {
-      const headers = this.createHeaders();
-      return this.http.get<GitHubRepository[]>(`${this.apiBase}/user/repos?per_page=100&type=all`, { headers })
+    const headers = this.createHeaders();
+    return this.http.get<GitHubRepository[]>(`${this.apiBase}/user/repos?per_page=100&type=all`, { headers })
   }
 
   getPullRequests(owner: string, repo: string): Observable<GitHubPullRequest[]> {
@@ -44,5 +77,17 @@ export class GithubService {
   getPullRequestDetails(owner: string, repo: string, pullNumber: number): Observable<GitHubPullRequest> {
     const headers = this.createHeaders();
     return this.http.get<GitHubPullRequest>(`${this.apiBase}/repos/${owner}/${repo}/pulls/${pullNumber}`, { headers });
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message
+        }`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
   }
 }
